a9a0b6aa2e9826c632cf8dae9aeb80b5ae986c56
YangJ00n <yangjoon820@gmail.com>
2022-02-14 22:35:36 +0900

    Setup

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const handleListen = () =>
+  console.log(`✅ Listening on http://localhost:${PORT}`);
+app.listen(PORT, handleListen);
