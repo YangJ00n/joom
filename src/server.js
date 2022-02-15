60063b18a5614a5f884c46e7497502db3a911c55
YangJ00n <yangjoon820@gmail.com>
2022-02-15 14:25:57 +0900

    WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -9,9 +11,27 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () =>
   console.log(`✅ Listening on http://localhost:${PORT}`);
-app.listen(PORT, handleListen);
+
+// http 서버 위에 WebSocket 서버를 만듦 -> 두 protocol이 같은 port를 공유함
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+  console.log("✅ Connected to Browser");
+
+  socket.on("close", () => {
+    console.log("❌ Disconnected from the Browser");
+  });
+  socket.on("message", (message) => {
+    console.log(message.toString());
+  });
+
+  socket.send("hello!!");
+});
+
+server.listen(3000, handleListen);
